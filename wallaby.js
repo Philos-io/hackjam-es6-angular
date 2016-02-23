ab24596d64e42df290280d8fdeaac920abe9461e
davyengone <engonenzedavy@yahoo.com>
2016-02-23 11:38:00 +0100

    fixed paths inside wallabyjs

--- wallaby.js
+++ wallaby.js
@@ -2,13 +2,13 @@ module.exports = function () {
   return {
     files: [
       'node_modules/chai/chai.js',
-      'workshops/01_controller_service_filter_directive_testing/solution/lib/angular.js',
+      'node_modules/angular/angular.js',
       'node_modules/angular-mocks/angular-mocks.js',
-      'workshops/01_controller_service_filter_directive_testing/solution/src/*.js',
+      'dist/bookstore.js',
       'workshops/01_controller_service_filter_directive_testing/solution/test/*.js'
     ],
     tests: [
-      'workshops/01_controller_service_filter_directive_testing/solution/tests/*.js'
+      'tests/*.spec.js'
     ]
   };
 };
